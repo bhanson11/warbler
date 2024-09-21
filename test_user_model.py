10768c9a101a36a554fb9361064057aa2d9f3660
Britten Hanson <britten.hanson11@gmail.com>
2024-09-21 11:23:35 -0600

    save for setup and teardown functions in test_user_model.py and then one of the follow methods

--- test_user_model.py
+++ test_user_model.py
@@ -35,11 +35,32 @@ class UserModelTestCase(TestCase):
     def setUp(self):
         """Create test client, add sample data."""
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        #drop all tables and then recreate them all for clean test environment between tests
+        db.drop_all()
+        db.create_all()
 
+        #create test users
+        u1 = User.signup("testuser1", "test1@test.com", "password", None)
+        u2 = User.signup("testuser2", "test2@test.com", "password", None)
+
+        #assign ID's to test users
+        u1.id = 1234
+        u2.id = 5678
+
+        db.session.commit()
+
+        #store users as variables for us in tests                
+        self.u1 = User.query.get(1234)
+        self.u2 = User.query.get(5678)
+
+        # set up test client
         self.client = app.test_client()
+    
+    def tearDown(self):
+        """teardown to reset to clean test environment"""
+        reset = super().tearDown()
+        db.session.rollback()
+        return reset
 
     def test_user_model(self):
         """Does basic model work?"""
@@ -56,3 +77,23 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+    ### test following
+
+    def test_user_follows(self):
+        """testing following functionality"""
+
+        # if test u1 follows u2 is that successful?
+        self.u1.following.append(self.u2)
+        db.session.commit()
+
+        # if u1 follows u2 does it show that u1 is following u2?
+        
+
+        # if u1 is not following u2 is that successfully detected?
+
+        # does u2 show u1 as a follower correctly?
+        
+        # does u2 show u1 not as a follower correctly? 
+        
+        # 
